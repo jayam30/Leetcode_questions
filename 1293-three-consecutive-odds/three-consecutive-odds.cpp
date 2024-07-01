e29234d57e82fa267bf38a232444e3dd98e389e8
Jayam pandit <94307580+jayam30@users.noreply.github.com>
2024-07-02 03:40:50 +0530

    Time: 5 ms (37.19%) | Memory: 10.6 MB (45.73%) - LeetSync

--- 1293-three-consecutive-odds/three-consecutive-odds.cpp
+++ 1293-three-consecutive-odds/three-consecutive-odds.cpp
@@ -1,19 +1,19 @@
+
+
 class Solution {
 public:
-    bool threeConsecutiveOdds(vector<int>& arr) {
-        int count = 0;  // Initialize count to keep track of consecutive odd numbers
-
-        for (int i = 0; i < arr.size(); i++) {  // Iterate through each element in the array
-            if (arr[i] % 2 != 0) {  // Check if the current element is odd
-                count++;  // Increment the count if it's odd
-                if (count == 3) {  // If we have found three consecutive odds, return true
+    bool threeConsecutiveOdds(std::vector<int>& arr) {
+        int cnt = 0;
+        for (int i = 0; i < arr.size(); i++) {   
+            if (arr[i] % 2 != 0) {
+                cnt++;
+                if (cnt == 3) {
                     return true;
                 }
-            } else {  // If the element is even, reset the count to 0
-                count = 0;
+            } else {
+                cnt = 0;
             }
         }
-
         return false;
     }
 };
