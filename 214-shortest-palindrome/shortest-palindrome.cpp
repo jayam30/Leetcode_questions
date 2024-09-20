da42d120b261477b31764de051335f3f227da5be
Jayam pandit <94307580+jayam30@users.noreply.github.com>
2024-09-20 17:35:17 +0530

    Time: 4 ms (80.10%) | Memory: 9.9 MB (83.37%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.cpp
+++ 214-shortest-palindrome/shortest-palindrome.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
     string shortestPalindrome(string s) {
-        int count = kmp(string(s.rbegin(), s.rend()), s);
-        return string(s.rbegin(), s.rend()).substr(0, s.length() - count) + s;
-    }
-
-private:
-    int kmp(const string &txt, const string &patt) {
-        string newString = patt + '#' + txt;
-        vector<int> pi(newString.length(), 0);
-        int i = 1, k = 0;
-        while (i < newString.length()) {
-            if (newString[i] == newString[k]) {
-                k++;
-                pi[i] = k;
+        int n = s.size();
+        
+        int i = 0; //  i = 0 to n-1
+        int j = n - 1; // j = n-1 to 0
+        
+        // Find the largest prefix palindrome
+        while (j >= 0) {
+            if (s[i] == s[j]) {
                 i++;
-            } else {
-                if (k > 0) {
-                    k = pi[k - 1];
-                } else {
-                    pi[i] = 0;
-                    i++;
-                }
             }
+            j--;
         }
-        return pi.back();
+        
+        // calculate remain
+        string remain = s.substr(i); // s trim from i to end
+        
+        // If the entire string is already a palindrome
+        // means remain = ""
+        if( remain.size()==0 ){ return s; }
+
+        string rev_remain = remain;
+        reverse(rev_remain.begin(), rev_remain.end());
+        
+        // Uncomment Below to see How LPP is printing for each step
+        // cout<< rev_remain <<" "<< s.substr(0,i) <<" "<< remain <<endl;
+        
+        // recurively calculate shortest palindrome in the longest prefix string
+        return rev_remain + shortestPalindrome(s.substr(0, i)) + remain;
     }
 };
